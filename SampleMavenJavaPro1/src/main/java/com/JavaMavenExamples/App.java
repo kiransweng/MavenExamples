e914a0800dc232cdb60b8a2b767d22350d6a384d
Kiran Kumar Edupuganti <IN090020@applabs.com>
2017-06-12 14:15:48 +0530

    Added new Maven Project

---
+++ SampleMavenJavaPro1/src/main/java/com/JavaMavenExamples/App.java
@@ -0,0 +1,25 @@
+package com.JavaMavenExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        final String str = "Hello!";
+        App appObject = new App();
+        System.out.println("The reverse string of \"" + str + "\" is \"" +
+                appObject.reverseString(str) + "\"");
+    }
+
+    public String reverseString(String src) {
+        return new StringBuilder(src).reverse().toString();
+
+    }
+
+
+}
